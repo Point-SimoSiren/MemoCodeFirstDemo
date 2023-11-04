58b287351b5c821f04163a4abeca7b4d7a10be55
Simo Sirén <simo.siren1@careeria.fi>
2023-11-04 23:32:18 +0200

    Add project files.

---
+++ MemoCodeFirstDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MemoCodeFirstDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MemoCodeFirstDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Memo> Memos { get; set; }
+    }
+}
